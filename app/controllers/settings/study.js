ba904f71120d2c86d9c2960d7af228190de77922
Omair Vaiyani <omair.vaiyani@live.co.uk>
2015-07-19 16:49:50 +0100

    Routine

--- app/controllers/settings/study.js
+++ app/controllers/settings/study.js
@@ -2,4 +2,35 @@ import Ember from 'ember';
 import CurrentUser from '../../mixins/current-user';
 
 export default Ember.Controller.extend(CurrentUser, {
+    init: function () {
+        var week = [];
+        _.each(this.get('daysOfTheWeek'), function (dayName) {
+            var day = {
+                label: dayName
+            };
+            _.each(this.get('slotsOfTheDay'), function (slot) {
+                day[slot.label] = {
+                    time: slot.times,
+                    disabled: true,
+                    activate: false
+                };
+            });
+            week.push(day);
+        }.bind(this));
+        this.get('week').clear();
+        this.get('week').addObjects(week);
+    },
+
+    daysOfTheWeek: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+
+    slotsOfTheDay: [{label: "morning", times: "8am - 11am"}, {label: "earlyAfternoon", times: "12pm - 2pm"},
+        {label: "lateAfternoon", times: "3pm - 5pm"}, {label: "evening", times: "7pm - 10pm"}],
+
+    week: new Ember.A(),
+
+    updateScheduleBasedOnIntensity: function () {
+        this.get('week').forEach(function (day) {
+
+        }.bind(this));
+    }.observes('currentUser.srIntensityLevel')
 });
