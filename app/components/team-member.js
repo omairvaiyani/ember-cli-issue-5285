184cd9521b2b63b542357c0d6620421e570543f3
Omair Vaiyani <omair.vaiyani@live.co.uk>
2015-10-31 14:29:48 +0000

    Codeship Error Check

--- app/components/team-member.js
+++ app/components/team-member.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     memberImageStyle: function () {
         var url = this.get('imageUrl');
-        return "background:url('"+url+"');";
+        return "background:url('" + url + "');";
     }.property("imageUrl"),
 
     twitterUrl: function () {
@@ -12,7 +12,7 @@ export default Ember.Component.extend({
 
     twitterTitle: function () {
         var firstName = this.get('name').split(" ")[0];
-        return "Follow "+firstName+" on Twitter!";
+        return "Follow " + firstName + " on Twitter!";
     }.property('name.length'),
 
     linkedInUrl: function () {
@@ -21,15 +21,29 @@ export default Ember.Component.extend({
 
     linkedInTitle: function () {
         var firstName = this.get('name').split(" ")[0];
-        return "Connect with "+firstName+" on Linkedin!";
+        return "Connect with " + firstName + " on Linkedin!";
     }.property('name.length'),
 
     emailTitle: function () {
         var firstName = this.get('name').split(" ")[0];
-        return "Send "+firstName+" an email!";
+        return "Send " + firstName + " an email!";
     }.property('name.length'),
 
     mailTo: function () {
-        return "mailto:"+this.get('email');
-    }.property('email.length')
+        return "mailto:" + this.get('email');
+    }.property('email.length'),
+
+    bio: function () {
+        if (this.get('memberType') === "founder") {
+            if (this.get('name') === "James Gupta")
+                return "A serial entrepreneur, passionate about improving education with technology. " +
+                    "James was previously the Technical Director for JumpIn, a student taxi sharing platform " +
+                    "acquired by Addison-Lee.";
+            else
+                return "An accomplished full-stack educational developer with " +
+                    "clients including the UK Medical Student's Association, Sweden's Karolinska Institutet " +
+                    "and Netherland's University Medical Centre."
+
+        }
+    }.property('memberType.length')
 });
