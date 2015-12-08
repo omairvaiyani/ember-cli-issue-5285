33dceba4f28005f4e68cd5eadad9513191e506e6
Omair Vaiyani <omair.vaiyani@live.co.uk>
2015-12-08 18:26:11 +0000

    Result Page: retry button added, attempt time/date added

--- app/helpers/simple-date.js
+++ app/helpers/simple-date.js
@@ -1,11 +1,17 @@
 import Ember from 'ember';
 
-function simpleDate(date) {
-    return new moment(date).format("MMM Do YYYY");
+function simpleDate(params) {
+    var date = params.hash.date,
+        format = params.hash.format;
+
+    if (!format && typeof format !== "string")
+        return new moment(date).format("MMM Do YYYY");
+    else
+        return new moment(date).format(format);
 }
 
 export {
     simpleDate
-    };
+};
 
 export default Ember.Handlebars.makeBoundHelper(simpleDate);
