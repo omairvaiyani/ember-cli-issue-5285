091ff6b27c3a44edc96d8c855ee851a3306d52c3
Omair Vaiyani <omair.vaiyani@live.co.uk>
2015-12-10 14:36:00 +0000

    Tags: end edit on outside click, overflow tags, max character count for tags added
    This handles a number of things:
    - Ends edit (either empties or adds new/updates old tag) when user
    clicks outside the tag
    - We can now set a max number of tags to display in certain places to
    avoid styling issues, this leads to a [+n] tag showing how many more
    tags the test has
    - Max length of 20 set on tags, to avoid ridiculously long tags, no
    styling magic can save a tag with 200 characters.
    - Max width adjustments for Edit Test header

--- app/components/module-tags.js
+++ app/components/module-tags.js
@@ -8,6 +8,41 @@ export default Ember.Component.extend({
     tags: null,
 
     /**
+     * @Property Show Max
+     * If > 0, only the max
+     * number of tags will be displayed,
+     * and an overflow tag will
+     * be added with a callback
+     * to display the rest.
+     */
+    showMax: 0,
+
+    /**
+     * @Property More Tags
+     * See 'Show Max' property.
+     * This is calculated in
+     */
+    moreTags: function () {
+        if (!this.get('showMax'))
+            return 0;
+
+        var totalTags = this.get('tags.length'),
+            showMax = this.get('showMax');
+
+        return totalTags - showMax;
+    }.property('tags.length'),
+
+    /**
+     * @Property Show Overflow Tags
+     * See 'Show Max' property.
+     * Displayed if 'More Tags'
+     * property is greater than 0.
+     */
+    showOverflowTag: function () {
+        return this.get('moreTags') > 0;
+    }.property('moreTags'),
+
+    /**
      * @Property {Ember.A<Ember.O>} Tag Array
      * This allows the component to
      * monitor the state of each
@@ -16,6 +51,12 @@ export default Ember.Component.extend({
      */
     tagArray: null,
 
+    /**
+     * @Property Tag or Module
+     * Mainly for placeholder text
+     * purposes: think onboarding
+     * module tags vs create test tags.
+     */
     tagOrModule: "Tag",
 
     /**
@@ -23,21 +64,26 @@ export default Ember.Component.extend({
      * See tagArray.
      */
     tagArrayUpdate: function () {
-        if(!this.get('tagArray'))
+        if (!this.get('tagArray'))
             this.set('tagArray', new Ember.A());
         else
             this.get('tagArray').clear();
 
         var tagArray = this.get('tagArray');
-        if(!this.get('tags.length')) {
+        if (!this.get('tags.length')) {
             return;
         }
-        var idIndex = 0;
-        this.get('tags').forEach(function (tag) {
+        var max = this.get('showMax');
+        this.get('tags').forEach(function (tag, index) {
             var tagObject = new Ember.Object();
             tagObject.set('tag', tag);
+            if (max) {
+                tagObject.set('show', index < max)
+            } else {
+                tagObject.set('show', true);
+            }
             tagObject.set('editingTag', false);
-            tagObject.set('id', "tag-" + idIndex++);
+            tagObject.set('id', "tag-" + index);
             tagArray.pushObject(tagObject);
         });
     }.observes('tags.length'),
@@ -53,6 +99,55 @@ export default Ember.Component.extend({
         this.tagArrayUpdate();
     }.on('init'),
 
+    autoEndNewTagInsertion: function () {
+        var parentController = this.get('parentController');
+
+        if (parentController.get('addingTag')) {
+            // set click event
+            setTimeout(function () {
+                $(document).click(function (event) {
+                    if (!$(event.target).closest('#new-tag-container').length) {
+                        if (parentController.get('addingTag')) {
+                            parentController.send('toggleAddingNewTag');
+                        }
+                    }
+                }.bind(this));
+            }, 200);
+
+        } else {
+            // off click event
+            $(document).off('click');
+        }
+    }.observes('parentController.addingTag'),
+
+    autoEndEditTag: function () {
+        var _this = this,
+            editingTag = false;
+
+        this.get('tagArray').forEach(function (tagObject) {
+            if (tagObject.get('editingTag'))
+                editingTag = true;
+        });
+
+        if (editingTag) {
+            // set click event
+            setTimeout(function () {
+                $(document).click(function (event) {
+                    if (!$(event.target).closest('#edit-tag-container').length) {
+                        if (editingTag) {
+                            editingTag = false;
+                            _this.send('finishEditingTag');
+                        }
+                    }
+                }.bind(this));
+            }, 200);
+
+        } else {
+            // off click event
+            $(document).off('click');
+        }
+    }.observes('tagArray.@each.editingTag'),
+
     actions: {
         tagClicked: function () {
             if (this.get('overrideAction'))
@@ -64,7 +159,8 @@ export default Ember.Component.extend({
         },
 
         toggleAddingNewTag: function () {
-            this.get('parentController').send('toggleAddingNewTag');
+            var parentController = this.get('parentController');
+            parentController.send('toggleAddingNewTag');
         },
 
         /**
@@ -73,8 +169,9 @@ export default Ember.Component.extend({
          */
         editTag: function (tagObject) {
             // Stop other tags from being edited first
-            this.send('finishEditingTag');
-
+            this.get('tagArray').forEach(function (tO) {
+                tO.set('editingTag', false);
+            });
             tagObject.set('editingTag', true);
             setTimeout(function () {
                 $("#" + tagObject.get('id')).focus();
@@ -85,11 +182,39 @@ export default Ember.Component.extend({
          * @Action Stop Editing Tag
          * Catch all, needed as param
          * not sent with input enter.
+         *
+         * Updates the 'tags' array
+         * fully, as tagArray.tagObject.tag
+         * does not update the parent
+         * tags array.
+         *
+         * Do not change this code.
          */
         finishEditingTag: function () {
+            var updatedTags = [];
             this.get('tagArray').forEach(function (tagObject) {
                 tagObject.set('editingTag', false);
-            });
+                updatedTags.push(tagObject.get('tag'));
+            }.bind(this));
+            this.get('tags').clear();
+            this.get('tags').addObjects(updatedTags);
+        },
+
+        /**
+         * @Action Show Overflow Tags
+         * See 'Show Max' property.
+         * Called by pressing the moreTags
+         * tag: showOverflowTagsAction
+         * redirects this action to the
+         * controller.
+         */
+        showOverflowTags: function () {
+            var action = this.get('showOverflowTagsAction') ? this.get('showOverflowTagsAction') :
+                this.get('overrideAction');
+            if (!action)
+                console.log("ModuleTagsComponent error, no showOverflowTagsAction defined.");
+            else
+                this.get('parentController').send(action);
         }
     }
 });
