a7569cb99f48622251854ba5b2770a22e8a8dde6
Omair Vaiyani <omair.vaiyani@live.co.uk>
2015-12-14 20:51:26 +0000

    Study Settings: added save button, only shown if changes found

--- app/controllers/settings/study.js
+++ app/controllers/settings/study.js
@@ -20,16 +20,30 @@ export default Ember.Controller.extend(CurrentUser, {
             this.send('activateSpacedRepetition');
     }.observes('currentUser.srActivated'),
 
-    // TODO update ember-data to make this work
-    /*currentUserDidDirty: function () {
-
-    }.observes('currentUser.isDirty'),*/
+    /**
+     * @Property Current User Did Dirty
+     * Checks current user for direct
+     * changes. Internal object changes
+     * are not monitored by default:
+     * only extra property to check
+     * is the do not disturb array -
+     * see actions.toggleSlot
+     */
+    currentUserDidDirty: function () {
+        return this.get('currentUser.isDirty') || this.get('srDoNotDisturbTimesAltered');
+    }.property('currentUser.isDirty', 'srDoNotDisturbTimesAltered'),
 
     studyIntensityLabel: function () {
         switch (this.get('currentUser.srIntensityLevel')) {
-            case "1": case 1: return "Light";
-            case "2": case 2: return "Moderate";
-            case "3": case 3: return "High";
+            case "1":
+            case 1:
+                return "Light";
+            case "2":
+            case 2:
+                return "Moderate";
+            case "3":
+            case 3:
+                return "High";
             default:
                 return "Moderate";
         }
@@ -47,18 +61,46 @@ export default Ember.Controller.extend(CurrentUser, {
                 .then(function () {
                     return this.get('currentUser').reload();
                 }.bind(this)).then(function () {
-                    this.set('preparingSpacedRepetition', false);
-                    this.send('decrementLoadingItems');
-                    this.get('currentUser').save();
-                }.bind(this), function (error) {
-                    console.dir(error);
-                    this.send('decrementLoadingItems');
-                }.bind(this));
+                this.set('preparingSpacedRepetition', false);
+                this.send('decrementLoadingItems');
+                this.get('currentUser').save();
+            }.bind(this), function (error) {
+                console.dir(error);
+                this.send('decrementLoadingItems');
+            }.bind(this));
         },
 
         toggleSlot: function (slotIndex, dayIndex) {
             var slot = this.get('currentUser.srDoNotDisturbTimes')[dayIndex].slots[slotIndex];
             Ember.set(slot, 'active', !slot.active);
+
+            // For isDirty/Saving purposes
+            this.set('srDoNotDisturbTimesAltered', true);
+        },
+
+        saveChanges: function (callback) {
+            var promise = this.get('currentUser').save().then(function () {
+                // For isDirty/Saving purposes
+                this.set('srDoNotDisturbTimesAltered', false);
+
+                var notification = {
+                    type: "saved",
+                    title: "Changes Saved!"
+                };
+
+                this.send('addNotification', notification);
+                window.scrollTo(0, 0)
+            }.bind(this), function (error) {
+                var notification = {
+                    type: "error",
+                    title: "Error!",
+                    message: error.error
+                };
+                this.send('addNotification', notification);
+            }.bind(this));
+
+            if(callback)
+                callback(promise);
         }
     }
 });
