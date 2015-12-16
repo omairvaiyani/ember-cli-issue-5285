cfba63808c4e6ab19ed3d2380289e892229e5834
Omair Vaiyani <omair.vaiyani@live.co.uk>
2015-12-16 17:57:22 +0000

    Added Save button to General Settings

--- app/controllers/settings.js
+++ app/controllers/settings.js
@@ -2,4 +2,27 @@ import Ember from 'ember';
 import CurrentUser from '../mixins/current-user';
 
 export default Ember.Controller.extend(CurrentUser, {
+    actions: {
+        saveChanges: function (callback) {
+            var promise = this.get('currentUser').save().then(function () {
+                var notification = {
+                    type: "saved",
+                    title: "Changes Saved!"
+                };
+
+                this.send('addNotification', notification);
+                window.scrollTo(0, 0)
+            }.bind(this), function (error) {
+                var notification = {
+                    type: "error",
+                    title: "Error!",
+                    message: error.error
+                };
+                this.send('addNotification', notification);
+            }.bind(this));
+
+            if(callback)
+                callback(promise);
+        }
+    }
 });
