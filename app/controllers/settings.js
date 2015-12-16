43a170665e6d26edd4c6365bc4c0293e6f75dd0e
Omair Vaiyani <omair.vaiyani@live.co.uk>
2015-12-16 18:54:06 +0000

    Settings Page: robust unsaved changes tracking and rolling back

--- app/controllers/settings.js
+++ app/controllers/settings.js
@@ -2,27 +2,44 @@ import Ember from 'ember';
 import CurrentUser from '../mixins/current-user';
 
 export default Ember.Controller.extend(CurrentUser, {
+    init: function () {
+        // Save any prior changes, so that if we have to 'rollback'
+        // changes here, we don't overshoot.
+        if (this.get('currentUser.isDirty'))
+            this.get('currentUser').save();
+    },
+
     actions: {
-        saveChanges: function (callback) {
-            var promise = this.get('currentUser').save().then(function () {
-                var notification = {
-                    type: "saved",
-                    title: "Changes Saved!"
-                };
 
-                this.send('addNotification', notification);
-                window.scrollTo(0, 0)
-            }.bind(this), function (error) {
+        /**
+         * @Action Discard and Continue
+         *
+         * Notification Confirmation callback.
+         *
+         * See SettingsRoute.actions.willTransition
+         *
+         * If user chooses discard, we rollback
+         * changes and continue with transition.
+         * Else, nothing happens and they stay
+         * on this route
+         *
+         * @param isPositive
+         * @param transition
+         */
+        discardAndContinue: function (isPositive, transition) {
+            this.set('transitionInLimbo', false);
+            if(isPositive) {
+                this.get('currentUser').rollback();
+
                 var notification = {
-                    type: "error",
-                    title: "Error!",
-                    message: error.error
+                    type: "warning",
+                    title: "Changes Reverted!"
                 };
                 this.send('addNotification', notification);
-            }.bind(this));
 
-            if(callback)
-                callback(promise);
+                var currentUser = this.get('currentUser');
+                transition.retry();
+            }
         }
     }
 });
--- app/routes/settings/study.js
+++ app/routes/settings/study.js
@@ -1,13 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    actions: {
-        // Struggling to get currentUser.isDirty
-        // So just save all changes
-        willTransition: function () {
-            var controller = this.controllerFor('settings.study');
-            controller.get('currentUser').save();
-            return true;
-        }
-    }
 });
