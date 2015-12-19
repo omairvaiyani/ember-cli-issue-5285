8a6957071e9d115603a46aace6da0f4e3cd0065c
Omair Vaiyani <omair.vaiyani@live.co.uk>
2015-12-19 16:26:27 +0000

    Profile Picture: border radius code improved, and UX changed

--- app/views/profile-image.js
+++ app/views/profile-image.js
@@ -13,17 +13,12 @@ export default Ember.View.extend({
         var url = this.get('user.profileImageURL'),
             style = "";
         if (url)
-             style += "background-image:url(" + url + ");";
-        if(this.get('height'))
-            style += "height:"+this.get('height')+"px;";
-        if(this.get('width'))
-            style += "width:"+this.get('width')+"px;";
+            style += "background-image:url(" + url + ");";
+        if (this.get('height'))
+            style += "height:" + this.get('height') + "px;";
+        if (this.get('width'))
+            style += "width:" + this.get('width') + "px;";
 
-//        if(this.get('border')) {
- //           if (this.get('border') !== 1) {
-            style += "border-color:#505050;border-style: solid;border-radius: 50%;"; // add border radius to the image
-  //          }
-   //     }
         return style;
     }.property('user')
 });
