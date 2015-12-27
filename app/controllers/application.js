57571b2e1493af5992f3ebaf0f6ffca7c767eb2e
Omair Vaiyani <omair.vaiyani@live.co.uk>
2015-12-27 21:52:14 +0000

    [Web][UX Improvement] Notifications dropdown scroll no longer scrolls the main page from inertia
    Learnt this from using Facebook

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -318,6 +318,7 @@ export default Ember.Controller.extend({
      */
     monitorActivityNotificationsDropDown: function () {
         var _this = this;
+
         $('.dropdown.activity-notifications').on({
             "shown.bs.dropdown": function () {
                 this.closable = true;
@@ -356,6 +357,45 @@ export default Ember.Controller.extend({
                 var closable = this.closable;
                 this.closable = true;
                 return closable;
+            }
+        });
+
+        /**
+         * @Listener
+         * @Class .parent-no-scroll
+         *
+         * Stops scroll propagation.
+         * When scrolling
+         * in the notifications dropdown,
+         * the inertia will not continue
+         * to the main page.
+         *
+         */
+        $('.parent-no-scroll').on('DOMMouseScroll mousewheel', function (ev) {
+            var $this = $(this),
+                scrollTop = this.scrollTop,
+                scrollHeight = this.scrollHeight,
+                height = $this.height(),
+                delta = (ev.type == 'DOMMouseScroll' ?
+                ev.originalEvent.detail * -40 :
+                    ev.originalEvent.wheelDelta),
+                up = delta > 0;
+
+            var prevent = function () {
+                ev.stopPropagation();
+                ev.preventDefault();
+                ev.returnValue = false;
+                return false;
+            };
+
+            if (!up && -delta > scrollHeight - height - scrollTop) {
+                // Scrolling down, but this will take us past the bottom.
+                $this.scrollTop(scrollHeight);
+                return prevent();
+            } else if (up && delta > scrollTop) {
+                // Scrolling up, but this will take us past the top.
+                $this.scrollTop(0);
+                return prevent();
             }
         });
     },
